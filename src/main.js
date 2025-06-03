a18afc32302c0e7e670627065086fdf2127c01e4
Oleh Shtembuliak <oleg.shtembulyack@gmail.com>
2025-06-03 21:31:37 +0800

    Complete main.js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.warning({ message: 'Please enter a search query!' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    if (data.hits.length === 0) {
+      iziToast.error({ message: 'Sorry, there are no images matching your search query. Please try again!' });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({ message: 'An error occurred while fetching data.' });
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+});
